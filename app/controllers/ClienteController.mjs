9994538ec6e1b3ddec1540303cb3b6d744d20aab
WillianChavez <williamchavezamaya@gmail.com>
2024-09-30 19:40:55 -0600

    feat(clientes): validacion de campos

--- app/controllers/ClienteController.mjs
+++ app/controllers/ClienteController.mjs
@@ -1,4 +1,6 @@
+import { Op } from 'sequelize';
 import { Cliente } from '../models/index.mjs';
+import BadRequestException from '../../handlers/BadRequestException.mjs';
 
 export default class ClienteController {
   static async list(req, res) {
@@ -24,6 +26,37 @@ export default class ClienteController {
   static async update(req, res) {
     const { nombre, dui, email } = req.body;
     const cliente = await Cliente.findByPk(req.params.id);
+
+    if (!cliente) {
+      throw new BadRequestException('Cliente no existe');
+    }
+
+    const existEmail = await Cliente.findOne({
+      where: {
+        email,
+        id: {
+          [Op.ne]: cliente.id,
+        },
+      },
+    });
+
+    if (existEmail) {
+      throw new BadRequestException('Email ya existe');
+    }
+
+    const existDui = await Cliente.findOne({
+      where: {
+        dui,
+        id: {
+          [Op.ne]: cliente.id,
+        },
+      },
+    });
+
+    if (existDui) {
+      throw new BadRequestException('DUI ya existe');
+    }
+
     cliente.nombre = nombre;
     cliente.dui = dui;
     cliente.email = email;
